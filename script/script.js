b8715971b1027ba97e8649c007f3d408436a51c6
Antonio Murilo <antoniomurilog@gmail.com>
2025-11-19 19:37:17 -0300

    node install

--- script/script.js
+++ script/script.js
@@ -2,6 +2,10 @@ let button = document.getElementById("button");
 let menu = document.querySelector(".esconder");
 let laser = document.querySelector(".laser");
 let links = document.querySelectorAll(".esconder a");
+let slids = document.querySelector(".slides")
+let cardsImagens = document.querySelectorAll(".img-cads")
+let prev = document.querySelector(".prev-btn")
+let next = document.querySelector(".next-btn")
 
 button.addEventListener("click", () => {
     if(menu.classList.contains("esconder")){
@@ -36,3 +40,10 @@ links.forEach(link => {
 menu.addEventListener("mouseleave", () => {
     laser.style.width = "0";
 });
+
+let index = 0;
+const cardWidth = cardsImagens[0].offsetWidth + 40;
+
+function updateSlid() {
+    slids.style.transform 
+}
