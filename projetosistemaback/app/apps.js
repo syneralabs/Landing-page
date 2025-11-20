9fa953d53605205333c56610325eeebd4ecbce1f
julio <juliona10@gmail.com>
2025-11-20 14:35:40 -0300

    alteraçoes
    alteraçaoes no routs serviço, apps e incremtçõ ao bdSynera

--- projetosistemaback/app/apps.js
+++ projetosistemaback/app/apps.js
@@ -1,39 +1,28 @@
-// iniciar o servidor, configurar as rotas, permitir que o front se conecte, fazer o json  processar, fazer a pone com o bdSinera
-<<<<<<< HEAD
-
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const bdSinera = require('./bd/bdSinera');
-=======
->>>>>>> main
-
-
 import express from 'express';
 import cors from 'cors';
 
-// Importando o banco (ES Module exige o .js)
+// Importando o banco
 import db from "../database/dbSynera.js";
 
-// Importando as rotas (também precisam terminar com .js)
+// Importando as rotas
 import clientesRoutes from "../routes/clientes.js";
 import servicosRoutes from "../routes/servicos.js";
-import pagamentosRoutes from "../routes/pagamentos.js"
+import pagamentosRoutes from "../routes/pagamentos.js";
 
-// Inicializando o Express (estava faltando)
+// Inicializando o Express
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//Rotas
+// Rotas
 app.use('/clientes', clientesRoutes);
 app.use('/servicos', servicosRoutes);
 app.use('/pagamentos', pagamentosRoutes);
 
 // Porta do servidor
 const Port = process.env.PORT || 3000;
+
 app.listen(Port, () => {
     console.log(`Servidor rodando na porta ${Port}`);
 });
-
