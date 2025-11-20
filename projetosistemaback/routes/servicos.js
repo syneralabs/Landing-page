c3680a4c9d2d388079bc59fe3314463c81df4533
julio <juliona10@gmail.com>
2025-11-20 15:06:28 -0300

    alteraçoes
    acrescentado ao riuts servico o editar serviço, deletar serviços

--- projetosistemaback/routes/servicos.js
+++ projetosistemaback/routes/servicos.js
@@ -44,4 +44,47 @@ router.get('/listar', (req, res) => {
     });
 });
 
+// editar serviço
+router.put('/editar/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, descricao, preco } = req.body;
+
+    const query = `
+        UPDATE servicos
+        SET nome = ?, descricao = ?, valor = ?
+        WHERE id = ?
+    `;
+
+    db.run(query, [nome, descricao, preco, id], function (err) {
+        if (err) {
+            console.error("Erro ao editar serviço:", err);
+            return res.status(500).json({ error: "Erro ao editar serviço" });
+        }
+
+        if (this.changes === 0) {
+            return res.status(404).json({ error: "Serviço não encontrado" });
+        }
+        res.status(200).json({ message: "Serviço atualizado com sucesso" });
+    });
+});
+
+// deletar serviço
+router.delete('/deletar/:id', (req, res) => {
+    const { id } = req.params;
+
+    const query = ` DELETE FROM servicos WHERE id = ?
+    `;
+    db.run(query, [id], function (err) {
+        if (err) {
+            console.error("Erro ao deletar serviço:", err);
+            return res.status(500).json({ error: "Erro ao deletar serviço" });
+        }
+
+        if (this.changes === 0) {
+            return res.status(404).json({ error: "Serviço não encontrado" });
+        }
+        res.status(200).json({ message: "Serviço deletado com sucesso" });
+    });
+});
+
 export default router;
